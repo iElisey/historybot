a2206b51de7ddac1b7130cd6d3c097bfb5a32494
iElisey <yelisei.osadchyi@gmail.com>
2024-08-18 13:49:50 +0300

    when making head request, responsing 200 OK

--- src/main/java/org/elos/historybot/HeadController.java
+++ src/main/java/org/elos/historybot/HeadController.java
@@ -1,7 +1,6 @@
 package org.elos.historybot;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
