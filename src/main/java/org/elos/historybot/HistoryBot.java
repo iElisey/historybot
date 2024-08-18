47c7dc024b90bf8058e9c620570f862f9fbacfa7
iElisey <yelisei.osadchyi@gmail.com>
2024-08-18 15:48:02 +0300

    fixing error with files when jar file

--- src/main/java/org/elos/historybot/HistoryBot.java
+++ src/main/java/org/elos/historybot/HistoryBot.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Component
 @PropertySource("classpath:application.properties")
